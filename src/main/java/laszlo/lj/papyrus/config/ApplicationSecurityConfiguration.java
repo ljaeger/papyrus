781a4b987b852d00c8699af10a5da48b3dea7434
Laszlo Jaeger <mrlaszlojaeger@gmail.com>
2020-02-12 15:53:05 +0100

    add in-memory users and role based restrictions

--- src/main/java/laszlo/lj/papyrus/config/ApplicationSecurityConfiguration.java
+++ src/main/java/laszlo/lj/papyrus/config/ApplicationSecurityConfiguration.java
@@ -1,22 +1,54 @@
 package laszlo.lj.papyrus.config;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                    .antMatchers("/scroll").hasRole("ADMIN")
+                    .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withUsername("demo").password(passwordEncoder.encode("demo")).roles("USER").build());
+        users.add(User.withUsername("admin").password(passwordEncoder.encode("secret"))
+                .roles("USER", "ADMIN").build());
+        return new InMemoryUserDetailsManager(users);
     }
 }
